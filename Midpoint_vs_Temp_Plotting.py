5354b155481f7728273a992dba010515aaeb2817
Michelle Wellman <51219249+MichelleWellman@users.noreply.github.com>
2020-05-13 20:22:21 -0400

    Cleaned up Midpoint vs. Temp Plotting

--- Midpoint_vs_Temp_Plotting.py
+++ Midpoint_vs_Temp_Plotting.py
@@ -19,22 +19,22 @@ sns.set()
 
 alphas_filename = 'alphas.h5'
 date_list = ['20190516', '20190424']
-bias_voltage = '47V'
-
-y_range_data = (0.1, 0.6)
-y_range_ratio = (0.1, 0.6)
 
 temperature_values = np.array([166, 167, 168, 169, 170, 171, 172])
 temperature_values_adjusted = temperature_values-169
 
 # Color Variables
-color_1 = '#FF5A8C'
-ecolor_1 = '#990F4C'
-txt_color_1 = '#E64676'
+color_27mm = '#FF5A8C'
+ecolor_27mm = '#990F4C'
+txt_color_27mm = '#E64676'
 
-color_2 = '#00C8E1'
-ecolor_2 = '#005F80'
-txt_color_2 = '#0086B3'
+color_38mm = '#00C8E1'
+ecolor_38mm = '#005F80'
+txt_color_38mm = '#0086B3'
+
+color_31mm = '#B073FF'
+ecolor_31mm = '#7800BF'
+txt_color_31mm = '#E64676'
 
 ratio_color = '#00B400'
 ratio_ecolor = '#007800'
@@ -73,6 +73,7 @@ def line_func(p, x):
 
 # Get the slope (a) and intercept (b) parameters that best fit the data.
 # Also returns the errors on the best fit parameters, and the covariance matrix.
+# The optimized parameters and the parameter errors form one large tuple.
 def get_fit_parameters(x, y, x_err, y_err):
     global odr
     model_func = odr.Model(line_func)
@@ -84,7 +85,7 @@ def get_fit_parameters(x, y, x_err, y_err):
     parameter_errors = out.sd_beta
     cov_matrix = out.cov_beta
 
-    return optimized_parameters, parameter_errors, cov_matrix
+    return (optimized_parameters, parameter_errors), cov_matrix
 
 # Calculate the reduced chi-squared value in one dimension (uses y-errors only).
 def calc_reduced_chisquare_1d(opt_parameters, x_vals, y_vals, y_errors):
@@ -109,48 +110,34 @@ def calc_reduced_chisquare_2d(opt_parameters, x_vals, y_vals, x_errors, y_errors
 # Plot the raw data points and the error bars, find and plot the best fit line.
 # Returns the optimized parameters, parameter errors, and covariance matrix for the best fit line, and both reduced chisquare values.
 def plot_data(df, color, ecolor, label):
-    x_range = np.array(range(166, 173))
-
     x = df[['temperature_avg']].values[:, 0]
     x_adjusted = x-169
-    x_range_adjusted = x_range-169
 
     y = df[['midpoint']].values[:, 0]
     x_error = df[['temperature_rms']].values[:, 0]
     y_error = df[['midpt_error']].values[:, 0]
 
     ax_data.errorbar(x_adjusted, y, y_error, x_error, ls='none', color=ecolor, barsabove=True, zorder=3)
-    
-    optimized_parameters, parameter_errors, cov_matrix = get_fit_parameters(x_adjusted, y, x_error, y_error)
 
-    best_fit_line = line_func(optimized_parameters, x_range_adjusted)
+    fit_parameters, cov_matrix = get_fit_parameters(x_adjusted, y, x_error, y_error)
+    optimized_parameters = fit_parameters[0]
+    best_fit_line = line_func(optimized_parameters, temperature_values_adjusted)
 
-    ax_data.plot(x_range_adjusted, best_fit_line, c=color, label=label, linewidth=0.8)
+    ax_data.plot(temperature_values_adjusted, best_fit_line, c=color, label=label, linewidth=0.8)
 
     reduced_chisquare_1d = calc_reduced_chisquare_1d(optimized_parameters, x_adjusted, y, y_error)
     reduced_chisquare_2d = calc_reduced_chisquare_2d(optimized_parameters, x_adjusted, y, x_error, y_error)
 
-    return optimized_parameters, parameter_errors, cov_matrix, reduced_chisquare_1d, reduced_chisquare_2d
+    return fit_parameters, best_fit_line, cov_matrix, reduced_chisquare_1d, reduced_chisquare_2d
 
-# Find the ratio of the data and return the slope, intercept, and errors on the slope and intercept of the best fit line.
-def get_ratio_line(optimized_parameters_1, optimized_parameters_2, temperatures):
-    # Find the best fit line for each separation
-    best_fit_line_1 = line_func(optimized_parameters_1, temperatures)
-    best_fit_line_2 = line_func(optimized_parameters_2, temperatures)
+# Find the errors on the ratio by propagating errors on the best fit lines.
+def get_ratio_errors(fit_parameters_1, fit_parameters_2, best_fit_line_1, best_fit_line_2, cov_matrix_1, cov_matrix_2, temperatures):
 
-    # Find the ratio line by taking the ratio of the best fit lines
-    ratio_line = best_fit_line_2/best_fit_line_1
-
-    return ratio_line
-
-# Find the errors on the ratio by propagating errors on the best fit lines
-# Make sure that the lower parameter value = variable_1, and higher parameter value = variable_2
-def get_ratio_errors(optimized_parameters_1, optimized_parameters_2, parameter_errors_1, parameter_errors_2, cov_matrix_1, cov_matrix_2, temperatures):
     # Variables representing the slope, intercept, and errors on the slope and intercept
-    (slope_1, intercept_1) = optimized_parameters_1
-    (slope_2, intercept_2) = optimized_parameters_2
-    (slope_error_1, intercept_error_1) = parameter_errors_1
-    (slope_error_2, intercept_error_2) = parameter_errors_2
+    optimized_parameters_1 = fit_parameters_1[0]
+    (slope_error_1, intercept_error_1) = fit_parameters_1[1]
+    optimized_parameters_2 = fit_parameters_2[0]
+    (slope_error_2, intercept_error_2) = fit_parameters_2[1]
 
     # Initializing lists for the expected voltages and ratio errors
     expected_voltages_1 = []
@@ -159,34 +146,31 @@ def get_ratio_errors(optimized_parameters_1, optimized_parameters_2, parameter_e
 
     # Loops through each integer temperature value, calculating the error on the ratio
     for temperature in temperatures:
+
         # Expected values at separation 1
-        expected_voltage_1 = slope_1*temperature + intercept_1
+        expected_voltage_1 = line_func(optimized_parameters_1, temperature)
         expected_voltages_1.append(expected_voltage_1)
 
         # Expected values at separation 2
-        expected_voltage_2 = slope_2*temperature + intercept_2
+        expected_voltage_2 = line_func(optimized_parameters_2, temperature)
         expected_voltages_2.append(expected_voltage_2)
 
         # Correlation parameters (rho)
         rho_1 = cov_matrix_1[0][1] / ( np.sqrt(cov_matrix_1[0][0]) * np.sqrt(cov_matrix_1[1][1]) )
         rho_2 = cov_matrix_2[0][1] / ( np.sqrt(cov_matrix_2[0][0]) * np.sqrt(cov_matrix_2[1][1]) )
 
-        # Best fit at the current temperature value
-        best_fit_1 = line_func(optimized_parameters_1, temperature)
-        best_fit_2 = line_func(optimized_parameters_2, temperature)
-
         # Errors on the best fit lins at the current temperature value
         best_fit_error_1 = np.sqrt( temperature**2 * slope_error_1**2 + intercept_error_1**2 + (2*rho_1 * slope_error_1 * temperature * intercept_error_1) )
         best_fit_error_2 = np.sqrt( temperature**2 * slope_error_2**2 + intercept_error_2**2 + (2*rho_2 * slope_error_2 * temperature * intercept_error_2) )
 
         # Calculate the ratio errors
-        ratio_error = (best_fit_2 / best_fit_1) * np.sqrt( (best_fit_error_2/best_fit_2)**2 + (best_fit_error_1/best_fit_1)**2 )
+        ratio_error = (expected_voltage_2 / expected_voltage_1) * np.sqrt( (best_fit_error_2/expected_voltage_2)**2 + (best_fit_error_1/expected_voltage_1)**2 )
         ratio_errors.append(ratio_error)
-    
-    # Plot the ratio errors
+
+    # Get the y-values for the ratio errors
     ratio_points = np.divide(expected_voltages_2, expected_voltages_1)
-    
-    return (slope_1, intercept_1, slope_error_1, intercept_error_1), (slope_2, intercept_2, slope_error_2, intercept_error_2), ratio_points, ratio_errors
+
+    return ratio_points, ratio_errors
 
 # Set the text strings for the plot
 def set_text_str(slope_label, intercept_label, slope, intercept, slope_error, intercept_error, reduced_chisquare_1d, reduced_chisquare_2d):
@@ -198,13 +182,58 @@ def set_text_str(slope_label, intercept_label, slope, intercept, slope_error, in
                         r'$\chi^2{2D}$' + f' = {reduced_chisquare_2d:.6f}'])
     return txt_str
 
-# General plot settings and functions 
-def plot_settings(df_1, df_2, separation_1, separation_2, fit_parameters_1, fit_parameters_2, reduced_chisquare_1d_1, reduced_chisquare_1d_2, reduced_chisquare_2d_1, reduced_chisquare_2d_2):
+#==================================================================================================
+### Executing the functions 
 
+# This if statement allows the code to run when the script is not being imported to another script.
+if __name__ == '__main__':
+
+    # Variables
+    bias_voltage = '47V'
+    y_range_data = (0.1, 0.6)
+    y_range_ratio = (0.1, 0.6)
+
+    # Defining the variables for the figure and both axes, setting the width of the figure.
+    fig, ax_data = plt.subplots()
+    fig.set_figwidth(9)
+    fig.set_figheight(6)
+    ax_ratio = ax_data.twinx()
+
+    # Create the dataframes
+    df_dates = create_df(alphas_filename)
+
+    df_27mm = get_final_df(df_dates, '27', bias_voltage)
+    df_38mm = get_final_df(df_dates, '38', bias_voltage)
+
+    # Deletes the first row of the dataframe only if the bias voltage is 48V and the separation is 27mm.
+    if bias_voltage ==  '48V':
+        df_27mm = df_27mm.iloc[1]
+    
+    # Plot the data and set the variables for the optimized parameters, parameter errors, covariance matrix, and reduced chi-square values.
+    fit_parameters_27mm, best_fit_line_27mm, cov_matrix_27mm, reduced_chisquare_1d_27mm, reduced_chisquare_2d_27mm = plot_data(df_27mm, color_27mm, ecolor_27mm, '27mm')
+    # optimized_parameters_27mm = fit_parameters_27mm[0]
+    # parameter_errors_27mm = fit_parameters_27mm[1]
+
+    fit_parameters_38mm, best_fit_line_38mm, cov_matrix_38mm, reduced_chisquare_1d_38mm, reduced_chisquare_2d_38mm = plot_data(df_38mm, color_38mm, ecolor_38mm, '38mm')
+    # optimized_parameters_38mm = fit_parameters_38mm[0]
+    # parameter_errors_38mm = fit_parameters_38mm[1]
+
+    # Find and plot the ratio line
+    ratio_line = best_fit_line_38mm/best_fit_line_27mm
+    ax_ratio.plot(temperature_values_adjusted, ratio_line, c=ratio_color, label='ratio', linewidth=0.75)
+
+    # Find and plot the ratio errors
+    ratio_points, ratio_errors = get_ratio_errors(fit_parameters_27mm, fit_parameters_38mm, best_fit_line_27mm, best_fit_line_38mm, cov_matrix_27mm, cov_matrix_38mm, temperature_values_adjusted)
+    print(ratio_errors)
+    ax_ratio.errorbar(temperature_values_adjusted, ratio_points, ratio_errors, ls='none', color=ratio_ecolor, barsabove=True, zorder=3)
+
+    #==============================================================================================
+    ## Plot settings
+    
     # Setting the y range for both axes
     ax_data.set_ylim(*y_range_data)
     ax_ratio.set_ylim(*y_range_ratio)
-    
+
     # Setting the axis labels
     ax_data.set_xlabel('Temperature [K]', fontsize=14)
     ax_data.set_ylabel('Midpoint [V]', fontsize=14)
@@ -213,22 +242,24 @@ def plot_settings(df_1, df_2, separation_1, separation_2, fit_parameters_1, fit_
     # Setting the super title and the title (the super title goes above the title and uses bigger text)
     plt.suptitle('Midpoint vs. Temperature at {} Bias Voltage\n'.format(bias_voltage), fontsize=14)
     
-    date_1 = ", ".join(df_1.date.unique())
-    date_2 = ", ".join(df_2.date.unique())
+    date_27mm = ", ".join(df_27mm.date.unique())
+    date_38mm = ", ".join(df_38mm.date.unique())
 
-    plt.title('{}mm: {}   {}mm: {}'.format(separation_1, date_1, separation_2, date_2))
-    
+    plt.title('27mm: {}   38mm: {}'.format(date_27mm, date_38mm))
+
     # Creating the text boxes
-    txtstr_1 = set_text_str('a', 'b', *fit_parameters_1, reduced_chisquare_1d_1, reduced_chisquare_2d_1)
-    txtstr_2 = set_text_str('c', 'd', *fit_parameters_2, reduced_chisquare_1d_2, reduced_chisquare_2d_2)
+    (slope_27mm, intercept_27mm), (slope_error_27mm, intercept_error_27mm) = fit_parameters_27mm
+    (slope_38mm, intercept_38mm), (slope_error_38mm, intercept_error_38mm) = fit_parameters_38mm
+    txtstr_27mm = set_text_str('a', 'b', slope_27mm, intercept_27mm, slope_error_27mm, intercept_error_27mm, reduced_chisquare_1d_27mm, reduced_chisquare_2d_27mm)
+    txtstr_38mm = set_text_str('c', 'd', slope_38mm, intercept_38mm, slope_error_38mm, intercept_error_38mm, reduced_chisquare_1d_38mm, reduced_chisquare_2d_38mm)
 
     # Setting the position of the text on the figure
     ax_data.set_position((0.1, 0.1, 0.6, 0.8))
     ax_ratio.set_position((0.1, 0.1, 0.6, 0.8))
-    plt.figtext(0.78, 0.5, txtstr_1, color=txt_color_1, fontsize=10)
-    plt.figtext(0.78, 0.25, txtstr_2, color=txt_color_2, fontsize=10)
+    plt.figtext(0.78, 0.5, txtstr_27mm, color=txt_color_27mm, fontsize=10)
+    plt.figtext(0.78, 0.25, txtstr_38mm, color=txt_color_38mm, fontsize=10)
 
-    # Label the x ticks with the actual temperature values (166-172)
+    # Label the x-ticks with the actual temperature values (166-172)
     for ax in [ax_data, ax_ratio]:
         locs = ax.get_xticks()
         adjusted_locs = [str(int(l+169)) for l in locs]
@@ -238,44 +269,3 @@ def plot_settings(df_1, df_2, separation_1, separation_2, fit_parameters_1, fit_
     ax_data.legend(bbox_to_anchor=(1.4, 1.0))
     ax_ratio.legend(bbox_to_anchor=(1.38, 0.9))
     plt.show()
-
-#==================================================================================================
-### Executing the functions 
-
-# This if statement runs the code when the script isn't being imported
-if __name__ == '__main__':
-
-    # Defining the variables for the figure and both axes. 
-    # Setting the width of the figure.
-    fig, ax_data = plt.subplots()
-    fig.set_figwidth(9)
-    fig.set_figheight(6)
-    ax_ratio = ax_data.twinx()
-    
-    # Create the dataframes
-    df_dates = create_df(alphas_filename)
-
-    df_27mm = get_final_df(df_dates, '27', bias_voltage)
-    df_38mm = get_final_df(df_dates, '38', bias_voltage)
-
-    # Deletes the first row of the dataframe only if the bias voltage is 48V and the separation is 27mm (it's a funky data point)
-    if bias_voltage =='48V':
-        df_27mm = df_27mm.iloc[1]
-    
-    # Plot the data and set the variables for the optimized parameters, parameter errors, cov matrix, and reduced chisquared values.
-    optimized_parameters_27mm, parameter_errors_27mm, cov_matrix_27mm, reduced_chisquare_1d_27mm, reduced_chisquare_2d_27mm = plot_data(df_27mm, color_1, ecolor_1, '27mm')
-    optimized_parameters_38mm, parameter_errors_38mm, cov_matrix_38mm, reduced_chisquare_1d_38mm, reduced_chisquare_2d_38mm = plot_data(df_38mm, color_2, ecolor_2, '38mm')
-
-    # Plot the ratio line
-    ratio_line = get_ratio_line(optimized_parameters_27mm, optimized_parameters_38mm, temperature_values_adjusted)
-    ax_ratio.plot(temperature_values_adjusted, ratio_line, c=ratio_color, label='ratio', linewidth=0.75)
-
-    # Plot the ratio errors
-    # slope_27mm, intercept_27mm, slope_error_27mm, intercept_error_27mm, slope_38mm, intercept_38mm, slope_error_38mm, intercept_error_38mm, ratio_points, ratio_errors = get_ratio_errors(optimized_parameters_27mm, optimized_parameters_38mm, parameter_errors_27mm, parameter_errors_38mm, cov_matrix_27mm, cov_matrix_38mm, temperature_values_adjusted)
-    fit_parameters_27mm, fit_parameters_38mm, ratio_points, ratio_errors = get_ratio_errors(optimized_parameters_27mm, optimized_parameters_38mm, parameter_errors_27mm, parameter_errors_38mm, cov_matrix_27mm, cov_matrix_38mm, temperature_values_adjusted)
-    ax_ratio.errorbar(temperature_values_adjusted, ratio_points, ratio_errors, ls='none', color=ratio_ecolor, barsabove=True, zorder=3)
-
-    # Plot settings
-    plot_settings(df_27mm, df_38mm, '27', '38', fit_parameters_27mm, fit_parameters_38mm, reduced_chisquare_1d_27mm, reduced_chisquare_1d_38mm, reduced_chisquare_2d_27mm, reduced_chisquare_2d_38mm)
-
-#==================================================================================================
