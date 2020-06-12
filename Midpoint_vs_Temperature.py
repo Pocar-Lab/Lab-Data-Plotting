83aa8655475768a184438d0b3b7c50c80a76494e
Michelle Wellman <51219249+MichelleWellman@users.noreply.github.com>
2020-06-12 19:24:20 -0400

    Added the residual subplot feature for Mid vs Temp plots

--- Midpoint_vs_Temperature.py
+++ Midpoint_vs_Temperature.py
@@ -27,10 +27,12 @@ from Functions import *
 ### Variables ###
 
 ## Change these variables to specify the desired conditions for the plot
-date_list = ['20181204', '20190228', '20181119', '20190207', '20181211']    # List the dates for all of the data you wish to include in the plot
+# date_list = ['20181204', '20190228', '20181119', '20190207', '20181211']    # List the dates for all of the data you wish to include in the plot
+date_list = ['20190207', '20181211', '20181204', '20190228']
 bias_voltage = '50V'                    # Enter the bias voltage you want to plot at
 separations = ['27', '31.07', '38']              # List the separation(s) you wish to include in the plot. For more than one separation
-plot_ratios = False                     # Indicate whether or not you want ratios included in on the plot 
+# residual_sub_plot = False                # Indicate whether or not you want to include the subplot of residuals
+plot_ratios = True                     # Indicate whether or not you want ratios included in on the plot 
 y_range_data = (0.0, 1.8)               # Set the range for the left y-axis (this sets the scale for the data)
 y_range_ratio = (0.1, 1.0)              # Set the range for the right y-axis (this sets the scale for the ratio)
 
@@ -42,21 +44,21 @@ data_colors = {
 }
 
 data_error_colors = {
-    '27': '#990F4C',
-    '31.07': '#CC6400',
+    '27': '#B40F4C',
+    '31.07': '#CB4B00',
     '38': '#7800BF',
 }
 
 ratio_colors = {
-    'ratio_1': '#00B400',
-    'ratio_2': '#00E1BE',
-    'ratio_3': '#00C8E1',
+    '27_31.07': '#00B400',
+    '27_38': '#00E1BE',
+    '31.07_38': '#0096FF',
 }
 
 ratio_error_colors = {
-    'ratio_1': '#007800',
-    'ratio_2': '#00A68C',
-    'ratio_3': '#005F80',
+    '27_31.07': '#007800',
+    '27_38': '#00A68C',
+    '31.07_38': '#005F80',
 }
 
 ## Unchanged variables
@@ -68,13 +70,12 @@ temperature_ints_shifted = temperature_ints-169
 ### Functions ###
 
 # Set text strings to display on the figure
-def set_text_str(slope_label, intercept_label, slope, intercept, slope_error, intercept_error, reduced_chisquare_1d, reduced_chisquare_2d):
+def set_text_str(slope_label, intercept_label, slope, intercept, slope_error, intercept_error, reduced_chisquare_2d):
     txt_str = '\n'.join(['Best fit function:',
                         'V = {}*(T-169) + {}'.format(slope_label, intercept_label),
                         'slope {} = {:.4f} +/- {:.4f}'.format(slope_label, slope, slope_error),
                         'intercept {} = {:.4f} +/- {:.4f}'.format(intercept_label, intercept, intercept_error),
-                        r'$\chi^2{1D}$' + f' = {reduced_chisquare_1d:.6f}',
-                        r'$\chi^2{2D}$' + f' = {reduced_chisquare_2d:.6f}'])
+                        r'$\chi^2_{2D}$' + f' = {reduced_chisquare_2d:.6f}'])
     return txt_str
 
 # Plot the raw data
@@ -87,17 +88,31 @@ def plot_data(ax, slope_label, intercept_label, df, separation, voltage, color, 
     optimized_parameters = fit_parameters[0]
     best_fit_line = linear_func(optimized_parameters, temperature_ints_shifted)
 
-    ax.errorbar(temps_shifted, midpoints, midpoint_errors, temp_errors, ls='none', color=ecolor, barsabove=True, zorder=3)
+    ax.errorbar(temps_shifted, midpoints, midpoint_errors, temp_errors, ls='none', color=ecolor, barsabove=True, zorder=3, label=label+' error')
     ax.plot(temperature_ints_shifted, best_fit_line, c=color, label=label, linewidth=0.8)
 
-    red_chisquare_1d = calc_red_chisquare_1d(optimized_parameters, temps_shifted, midpoints, midpoint_errors)
+    # red_chisquare_1d = calc_red_chisquare_1d(optimized_parameters, temps_shifted, midpoints, midpoint_errors)
     red_chisquare_2d = calc_red_chisquare_2d(optimized_parameters, temps_shifted, midpoints, temp_errors, midpoint_errors)
 
     (slope, intercept) = fit_parameters[0]
     (slope_error, intercept_error) = fit_parameters[1]
-    txt_str = set_text_str(slope_label, intercept_label, slope, intercept, slope_error, intercept_error, red_chisquare_1d, red_chisquare_2d)
+    txt_str = set_text_str(slope_label, intercept_label, slope, intercept, slope_error, intercept_error, red_chisquare_2d)
 
-    return fit_parameters, cov_matrix, txt_str
+    return fit_parameters, temps_shifted, midpoints, cov_matrix, txt_str
+
+# Plot the residuals subplot
+def plot_residuals(ax, temperatures, fit_parameters, midpoints, color, ecolor, separation):
+    y_expected_vals = []
+
+    for temp in temperatures:
+        y_expected = linear_func(fit_parameters, temp)
+        y_expected_vals.append(y_expected)
+    
+    residuals = midpoints - y_expected_vals
+    zeros = [0]*len(temperature_ints_shifted)
+
+    ax.plot(temperature_ints_shifted, zeros, c=color, linewidth=0.8)
+    ax.scatter(temperatures, residuals, c=ecolor, marker='.', label='{}mm residual'.format(separation))
 
 #==========================================================================================================
 ### Executing Functions ###
@@ -109,9 +124,10 @@ if __name__ == '__main__':
     # Define preliminary variables for plotting
     df_all_data = compile_data(alphas_filename, date_list)
 
-    fig, ax_data = plt.subplots(1, 1, figsize=(9, 6))
-    ax_data.set_position((0.1, 0.1, 0.6, 0.8))
-    axes = [ax_data]
+    fig, (ax_data, ax_sub) = plt.subplots(2, 1, sharex=False, figsize=(10, 7))
+    ax_data.set_position((0.08, 0.4, 0.6, 0.5))
+    ax_sub.set_position((0.08, 0.1, 0.6, 0.2))
+    axes = [ax_data, ax_sub]
 
     plot_suptitle = 'Midpoint vs. Temperature at {} Bias Voltage\n'.format(bias_voltage)
     plot_title = ''
@@ -121,16 +137,20 @@ if __name__ == '__main__':
         dataframe = create_df(df_all_data, separations[0], bias_voltage)
         
         # Delete the data point that's been mislabeled as 50V
-        # dataframe = dataframe[dataframe['midpoint'] > 1.0]
+        dataframe = dataframe[dataframe['midpoint'] > 1.0]
 
-        fit_parameters, cov_matrix, txt_str = plot_data(ax_data, 'a', 'b', dataframe, separations[0], bias_voltage, data_colors[separations[0]], data_error_colors[separations[0]], label='{}mm'.format(separations[0]))
+        # Plot the raw data and the best fit line
+        fit_parameters, temps_shifted, midpoints, cov_matrix, txt_str = plot_data(ax_data, 'a', 'b', dataframe, separations[0], bias_voltage, data_colors[separations[0]], data_error_colors[separations[0]], label='{}mm'.format(separations[0]))
+
+        # Plot the residuals on a subplot below the main plot
+        plot_residuals(ax_sub, temps_shifted, fit_parameters[0], midpoints, data_colors[separations[0]], data_error_colors[separations[0]], separations[0])
 
         # Setting the super title and the title variables
-        plot_suptitle = ('Midpoint vs. Temperature at {} Bias Voltage, {}mm Separation'.format(bias_voltage, separations[0]))
         plot_title = ", ".join(dataframe.date.unique())
+        plot_suptitle = ('Midpoint vs. Temperature at {} Bias Voltage, {}mm Separation'.format(bias_voltage, separations[0]))
         
         # Creating the text box
-        plt.figtext(0.78, 0.5, txt_str, color=data_colors[separations[0]], fontsize=8)
+        plt.figtext(0.75, 0.5, txt_str, color=data_colors[separations[0]], fontsize=10)
 
 
     elif len(separations) == 2:
@@ -143,8 +163,12 @@ if __name__ == '__main__':
         df_small_sep = df_small_sep[df_small_sep['midpoint'] > 1.0]
 
         # Call the function to plot the data, and return the fit parameters, cov matrix, and text string
-        fit_parameters_small, cov_matrix_small, txt_str_small = plot_data(ax_data, 'a', 'b', df_small_sep, separations[0], bias_voltage, data_colors[separations[0]], data_error_colors[separations[0]], label='{}mm'.format(separations[0]))
-        fit_parameters_large, cov_matrix_large, txt_str_large = plot_data(ax_data, 'c', 'd', df_large_sep, separations[1], bias_voltage, data_colors[separations[1]], data_error_colors[separations[1]], label='{}mm'.format(separations[1]))
+        fit_parameters_small, temps_shifted_small, midpoints_small, cov_matrix_small, txt_str_small = plot_data(ax_data, 'a', 'b', df_small_sep, separations[0], bias_voltage, data_colors[separations[0]], data_error_colors[separations[0]], label='{}mm'.format(separations[0]))
+        fit_parameters_large, temps_shifted_large, midpoints_large, cov_matrix_large, txt_str_large = plot_data(ax_data, 'c', 'd', df_large_sep, separations[1], bias_voltage, data_colors[separations[1]], data_error_colors[separations[1]], label='{}mm'.format(separations[1]))
+
+        # Plot the residuals on a subplot below the main plot
+        plot_residuals(ax_sub, temps_shifted_small, fit_parameters_small[0], midpoints_small, 'black', data_colors[separations[0]], separations[0])
+        plot_residuals(ax_sub, temps_shifted_large, fit_parameters_large[0], midpoints_large, 'black', data_colors[separations[1]], separations[1])
 
         # Setting the plot title variable
         date_small = ", ".join(df_small_sep.date.unique())
@@ -152,19 +176,24 @@ if __name__ == '__main__':
         plot_title = '{}mm: {}     {}mm: {}'.format(separations[0], date_small, separations[1], date_large)
 
         # Setting the positions of the text on the figure
-        plt.figtext(0.78, 0.5, txt_str_small, color=data_colors[separations[0]], fontsize=10)
-        plt.figtext(0.78, 0.25, txt_str_large, color=data_colors[separations[1]], fontsize=10)
+        if not plot_ratios:
+            plt.figtext(0.75, 0.55, txt_str_small, color=data_colors[separations[0]], fontsize=10)
+            plt.figtext(0.75, 0.35, txt_str_large, color=data_colors[separations[1]], fontsize=10)
 
         if plot_ratios:
 
             ax_ratio = ax_data.twinx()
-            ax_ratio.set_position((0.1, 0.1, 0.6, 0.8))
-            axes = [ax_data, ax_ratio]
+            ax_ratio.set_position((0.1, 0.4, 0.6, 0.5))
+            axes = [ax_data, ax_sub, ax_ratio]
 
             # Find and plot ratios and the ratio errors
             ratio_yvalues, ratio_line, ratio_errors = get_ratio_errors(fit_parameters_small, fit_parameters_large, cov_matrix_small, cov_matrix_large, temperature_ints_shifted)
-            ax_ratio.plot(temperature_ints_shifted, ratio_line, c=ratio_colors['ratio_1'], label='ratio {}mm/{}mm'.format(separations[1], separations[0]), linewidth=0.75)
+            ax_ratio.plot(temperature_ints_shifted, ratio_line, c=ratio_colors['ratio_1'], label='\n'.join(['ratio', '{}mm/{}mm']).format(separations[1], separations[0]), linewidth=0.75)
             ax_ratio.errorbar(temperature_ints_shifted, ratio_yvalues, ratio_errors, ls='none', color=ratio_error_colors['ratio_1'], barsabove=True, zorder=3)
+
+            # Setting the positions of the text on the figure
+            plt.figtext(0.78, 0.55, txt_str_small, color=data_colors[separations[0]], fontsize=10)
+            plt.figtext(0.78, 0.35, txt_str_large, color=data_colors[separations[1]], fontsize=10)
 
 
     elif len(separations) == 3:
@@ -183,8 +212,9 @@ if __name__ == '__main__':
         parameter_labels = [['a', 'b'], ['c', 'd'], ['e', 'f']]     # List of the differnet parameter labels to use for the text string
         par_dict = dict()       # The dictionary the outputs in the plot_data function will be saved to 
         for i, sep in enumerate(separations):
-            fit_parameters, cov_matrix, txt_str = plot_data(ax_data, parameter_labels[i][0], parameter_labels[i][1], dataframes[i], sep, bias_voltage, data_colors[sep], data_error_colors[sep], label='{}mm'.format(sep))
+            fit_parameters, temps_shifted, midpoints, cov_matrix, txt_str = plot_data(ax_data, parameter_labels[i][0], parameter_labels[i][1], dataframes[i], sep, bias_voltage, data_colors[sep], data_error_colors[sep], label='{}mm'.format(sep))
             par_dict[sep] = {'fit_parameters': fit_parameters, 'cov_matrix': cov_matrix, 'txt_str': txt_str}
+            plot_residuals(ax_sub, temps_shifted, fit_parameters[0], midpoints, 'black', data_colors[sep], sep)
 
         # Setting the plot title variable
         dates = []
@@ -194,21 +224,30 @@ if __name__ == '__main__':
         plot_title = '     '.join(dates)
 
         # Setting the positions of the text on the figure
-        plt.figtext(0.78, 0.5, par_dict[separations[0]]['txt_str'], color=data_colors[separations[0]], fontsize=10)
-        plt.figtext(0.78, 0.3, par_dict[separations[1]]['txt_str'], color=data_colors[separations[1]], fontsize=10)
-        plt.figtext(0.78, 0.1, par_dict[separations[2]]['txt_str'], color=data_colors[separations[2]], fontsize=10)
+        if not plot_ratios:
+            plt.figtext(0.75, 0.55, par_dict[separations[0]]['txt_str'], color=data_colors[separations[0]], fontsize=9)
+            plt.figtext(0.75, 0.38, par_dict[separations[1]]['txt_str'], color=data_colors[separations[1]], fontsize=9)
+            plt.figtext(0.75, 0.21, par_dict[separations[2]]['txt_str'], color=data_colors[separations[2]], fontsize=9)
 
         # Plot the ratios if the plot_ratios variable is set to True
         if plot_ratios:
 
             ax_ratio = ax_data.twinx()
-            ax_ratio.set_position((0.1, 0.1, 0.6, 0.8))
+            ax_ratio.set_position((0.08, 0.4, 0.6, 0.5))
+            axes = [ax_data, ax_sub, ax_ratio]
 
             # Find and plot the ratios and the ratio errors
             for a, b in combinations(separations, 2):       # Iterate through every combination of two separation values - a & b represent two possible separation values.
+                
                 ratio_yvalues_ab, ratio_line_ab, ratio_errors_ab = get_ratio_errors(par_dict[a]['fit_parameters'], par_dict[b]['fit_parameters'], par_dict[a]['cov_matrix'], par_dict[b]['cov_matrix'], temperature_ints_shifted)
-                ax_ratio.plot(temperature_ints_shifted, ratio_line_ab, c=ratio_colors['ratio_1'], label='ratio {}mm/{}mm'.format(b, a), linewidth=0.75)
-                ax_ratio.errorbar(temperature_ints_shifted, ratio_yvalues_ab, ratio_errors_ab, ls='none', color=ratio_error_colors['ratio_1'], barsabove=True, zorder=3)
+                ax_ratio.plot(temperature_ints_shifted, ratio_line_ab, c=ratio_colors[f'{a}_{b}'], label='ratio {}mm/{}mm'.format(b, a), linewidth=0.75)
+                ax_ratio.errorbar(temperature_ints_shifted, ratio_yvalues_ab, ratio_errors_ab, ls='none', color=ratio_error_colors[f'{a}_{b}'], barsabove=True, zorder=3)
+
+                # Setting the positions of the text on the figure
+                plt.figtext(0.78, 0.55, par_dict[separations[0]]['txt_str'], color=data_colors[separations[0]], fontsize=9)
+                plt.figtext(0.78, 0.38, par_dict[separations[1]]['txt_str'], color=data_colors[separations[1]], fontsize=9)
+                plt.figtext(0.78, 0.21, par_dict[separations[2]]['txt_str'], color=data_colors[separations[2]], fontsize=9)
+
 
         # =============================================================
 
@@ -261,6 +300,8 @@ if __name__ == '__main__':
     # Setting the axis labels
     ax_data.set_xlabel('Temperature [K]', fontsize=14)
     ax_data.set_ylabel('Midpoint [V]', fontsize=14)
+    ax_sub.set_xlabel('Temperature [K]', fontsize=14)
+    ax_sub.set_ylabel('Residuals [V]', fontsize=14)
 
     # Label the x-ticks with the actual temperature values (166-172)
     for ax in axes:
@@ -269,15 +310,15 @@ if __name__ == '__main__':
         ax.set_xticklabels(adjusted_locs)
 
     # Setting the super title and the title
-    plt.suptitle(plot_suptitle)
-    plt.title(plot_title)
+    plt.suptitle('\n'.join([plot_suptitle, plot_title]), fontsize=12)
 
     # Settings for the ratio plot
     if plot_ratios:
         ax_ratio.set_ylim(y_range_ratio)
         ax_ratio.set_ylabel('Ratio', fontsize=14)
-        ax_ratio.legend(bbox_to_anchor=(1.436, 0.9))
-
-    plt.grid(True)
+        ax_ratio.legend(bbox_to_anchor=(1.55, 1.0))
+        ax_ratio.grid(False)
+    
     ax_data.legend(bbox_to_anchor=(1.3, 1.0))
+    ax_sub.legend(bbox_to_anchor=(1.35, 0.5))
     plt.show()
